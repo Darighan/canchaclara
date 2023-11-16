fc5926f3fca03bea7c0084eddf84a6a43ff7a693
Sergio Carey <sergiocareyhola@gmail.com>
2023-11-16 02:06:14 -0300

    asdfasd

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class="Contenedor central">
-        <div class="Contenedor-Deportista">
-            <router-link :to="{ name: 'arriendos' }">
-                <v-btn>
-                    Login Deportista
-                </v-btn>
-            </router-link>
-
-        </div>
-        <div class="Contenedor-Empresa">
-            <router-link :to="{ name: 'empresas' }">
-                <v-btn>
-                    Login Empresa
-                </v-btn>
-            </router-link>
-        </div>
-        <div class="Contenedor-Administracion">
-            <router-link :to="{ name: 'administracion' }">
-                <v-btn>
-                    Login Administracion CanchaClara
-                </v-btn>
-            </router-link>
-        </div>
+    <div class="bg-img-home">
 
 
-      
+        <div class="Contenedor central">
+            <div class="Contenedor-Deportista">
+                <router-link :to="{ name: 'arriendos' }">
+                    <v-btn>
+                        Login Deportista
+                    </v-btn>
+                </router-link>
+
+            </div>
+            <div class="Contenedor-Empresa">
+                <router-link :to="{ name: 'empresas' }">
+                    <v-btn>
+                        Login Empresa
+                    </v-btn>
+                </router-link>
+            </div>
+            <div class="Contenedor-Administracion">
+                <router-link :to="{ name: 'administracion' }">
+                    <v-btn>
+                        Login Administracion CanchaClara
+                    </v-btn>
+                </router-link>
+            </div>
+        </div>
+
+
     </div>
 </template>
 
