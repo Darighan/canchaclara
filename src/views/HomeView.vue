a1bf2791dcae4909c7678b291e1bfd11af7ef95e
Sergio Carey <sergiocareyhola@gmail.com>
2023-11-27 03:20:10 -0300

    Estilando Fondos TestV1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,42 @@
 <template>
-    <div class="bg-img-home">
-      <div class="contenedor-central">
-        <!-- Contenedor de bienvenida -->
-        <div class="contenedor-bienvenida">
-          <h1>Bienvenido a CanchaClara</h1>
-          <div class="opcion-arriendo">
-            <p>¿Deseas realizar un arriendo?</p>
-            <router-link :to="{ name: 'arriendos' }">
-              <v-btn>haz click AQUÍ</v-btn>
-            </router-link>
-          </div>
-          <div class="opcion-complejo">
-            <p>¿Eres un Complejo Deportivo y Quieres Unirtenos?</p>
-            <router-link :to="{ name: 'empresas' }">
-              <v-btn>Haz click AQUÍ</v-btn>
-            </router-link>
-          </div>
-          <div class="opcion-administrador">
-            <p>¿Eres un Administrador?</p>
-            <router-link :to="{ name: 'administracion' }">
-              <v-btn>Haz click AQUÍ</v-btn>
-            </router-link>
-          </div>
-        </div>
+  <div class="bg-img-home">
+    <div class="contenedor-central">
+      <div class="titulo-central">
+        <h1>Bienvenido a CanchaClara</h1>
+      </div>
+      <div class="opciones">
+        <!-- Opción de arriendo que redirige a LoginView -->
+        <router-link to="/login" class="opcion">
+          <i class="icono fas fa-futbol"></i>
+          <p>¿Deseas realizar un arriendo?</p>
+          <p>HAZ CLICK ACA</p>
+        </router-link>
+        <!-- Opción de unirse que redirige a LoginEmpresasView -->
+        <router-link to="/empresas" class="opcion">
+          <i class="icono fas fa-building"></i>
+          <p>¿Eres un Complejo Deportivo y Quieres Unirtenos?</p>
+          <p>HAZ CLICK ACA</p>
+        </router-link>
+        <!-- Opción de administrador que redirige a LoginAdministracionView -->
+        <router-link to="/administracion" class="opcion">
+          <i class="icono fas fa-user-shield"></i>
+          <p>¿Eres un Administrador?</p>
+          <p>HAZ CLICK ACA</p>
+        </router-link>
       </div>
     </div>
-  </template>
-  
-  <script setup>
-  // Tu script de Vue aquí
-  </script>
-  
-  <style scoped>
-  @import "../assets/styles/homeViewStyle.scss";
-  </style>
+  </div>
+</template>
+
+<script setup>
+// Script setup de Vue es opcional para lógica adicional o estado del componente
+</script>
+
+<style scoped>
+@import "../assets/styles/homeViewStyle.scss";
+/* Estilos adicionales específicos para este componente si son necesarios */
+</style>
+
+
   
   
